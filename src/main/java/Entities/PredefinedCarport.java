debce4ca5d8d86177c16ac260f468b1168de678c
Snoopsie1 <oliver.rasoli@gmail.com>
2021-12-02 23:22:05 +0100

    added PDCarport entity

---
+++ src/main/java/Entities/PredefinedCarport.java
@@ -0,0 +1,46 @@
+package Entities;
+
+public class PredefinedCarport {
+    int width;
+    int length;
+    int id;
+
+
+    public PredefinedCarport(int width,int length){
+        this.width = width;
+        this.length = length;
+    }
+    public PredefinedCarport(int id){
+        this.id = id;
+    }
+
+    public PredefinedCarport(int width, int length, int id) {
+        this.width = width;
+        this.length = length;
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
